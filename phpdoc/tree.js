a214c7f1bacaddaba08fa96d8b9ff9f2743e6077
piwi <me@e-piwi.fr>
2015-03-02 19:52:30 +0100

    package's API documentation

---
+++ phpdoc/tree.js
@@ -0,0 +1,13 @@
+var tree = [["HttpFundamental","HttpFundamental.html","",
+                [["ContentType","HttpFundamental\/ContentType.html","",
+                [["Html","HttpFundamental\/ContentType\/Html.html","",
+                [        ]],["Json","HttpFundamental\/ContentType\/Json.html","",
+                [        ]],["PlainText","HttpFundamental\/ContentType\/PlainText.html","",
+                [        ]],["Xml","HttpFundamental\/ContentType\/Xml.html","",
+                [        ]]        ]],["ContentType","HttpFundamental\/ContentType.html","",
+                [        ]],["ContentTypeInterface","HttpFundamental\/ContentTypeInterface.html","",
+                [        ]],["Cookie","HttpFundamental\/Cookie.html","",
+                [        ]],["Request","HttpFundamental\/Request.html","",
+                [        ]],["Response","HttpFundamental\/Response.html","",
+                [        ]]        ]]        ]
+
